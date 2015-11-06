8891793aab4ca36067babde2f62f8c21acd3ddcf
Jeff Lyon <jeff@albatrossdigital.com>
2015-11-06 14:33:26 -0800

    Add Google search integration

--- templates/proud-search-form.tpl.php
+++ templates/proud-search-form.tpl.php
@@ -1,7 +1,12 @@
 <form class="form-inline get-started search-form align-left" id="wrapper-search" style="margin-top:30px" action="<?php print $action; ?>">
 
 <div class="input-group">
-  <input id="proud-search-input" class="form-control input-lg" type="text" placeholder="<?php print $placeholder; ?>" name="<?php print $name; ?>">
+  <input 
+    id="proud-search-input" class="form-control input-lg" type="text" autocomplete="off"
+    placeholder="<?php print $placeholder; ?>" 
+    name="<?php print $name; ?>" 
+    value="<?php print $query; ?>"
+  >
   <span class="input-group-btn">
     <button type="submit" value="Go" class="btn btn-primary btn-lg" id="proud-search-submit"><i class="fa fa-search"></i></button>
   </span>
